00833230ebd88861fff663184f1eaf62c3451fd7
windwize <91597193+windwize@users.noreply.github.com>
2022-05-17 10:08:44 +0900

    1

---
+++ main.py
@@ -0,0 +1,46 @@
+
+import argparse
+from ACO import ACO
+
+from utils import random_init,calculate_distance,load_example
+
+def default_argument_parser():
+    parser = argparse.ArgumentParser(description="Ant Colony Algorithm")
+    parser.add_argument("--test",nargs="?")
+    parser.add_argument('--ant', default=10,type=int)
+    parser.add_argument('--points', default=20,type=int)
+    parser.add_argument('--generation', default=100,type=int) #[]
+    parser.add_argument('--alpha', default=2.0,type=float)     #[1,4]
+    parser.add_argument('--beta', default=3.0,type=float)     #[0,5]
+    parser.add_argument('--rho', default=0.5,type=float)       #[0.2,0.5]
+    parser.add_argument('--q', default=100,type=float)
+    parser.add_argument('--strategy', default=2,type=int) 
+    parser.add_argument('--min_x', default=0,type=int) 
+    parser.add_argument('--max_x', default=50,type=int) 
+    parser.add_argument('--min_y', default=0,type=int) 
+    parser.add_argument('--max_y', default=100,type=int)
+    return parser
+
+def main():
+    args = default_argument_parser().parse_args()
+
+    if args.test!=None:
+        points,distance = load_example(args.test)
+    else:
+        points = random_init(args.points,args.min_x,args.max_x,args.min_y,args.max_y)
+        distance = calculate_distance(points)
+    aco = ACO(
+        ant_count=args.ant,
+        generations=args.generation,
+        alpha=args.alpha,
+        beta=args.beta,
+        rho=args.rho,
+        q=args.q,
+        strategy=args.strategy,
+        points=points,
+        distance=distance,
+    )
+    aco.run()
+
+if __name__ == '__main__':
+    main()
---
+++ utils.py
@@ -0,0 +1,119 @@
+
+import random
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+
+def random_init(points_num,min_x, max_x, min_y, max_y):
+    points = []
+
+    while len(points)!=points_num:
+        x = random.randint(min_x,max_x)
+        y = random.randint(min_y,max_y)
+        if [x,y] in points:
+            continue
+        points.append([x,y])
+    
+    file = open("temp.txt","w")
+    for i in range(len(points)):
+        file.write(f'{points[i][0]} {points[i][1]}\n')
+    file.close()
+    return points
+
+def dis(point1,point2):
+    return math.sqrt((point1[0]-point2[0]) ** 2 + (point1[1] - point2[1]) ** 2)#距離計算
+
+def calculate_distance(points):
+    distance = []
+    for i in range(len(points)):
+        list = []
+        for j in range(len(points)):
+            list.append(dis(points[i],points[j]))
+        distance.append(list)
+    return distance
+    
+def get_next_pos(possibility):
+    #print(possibility)
+    n = sum(possibility)
+    
+    for i in range(len(possibility)):
+        possibility[i]/=n
+    
+    n = sum(possibility)
+    r = random.uniform(0,n)
+    pos = 0
+    while True:
+        if possibility[pos]==0:
+            pos+=1
+        elif r-possibility[pos]<0:
+            return pos
+        else:
+            r-=possibility[pos]
+            pos=pos+1
+
+def init_pos(rank):
+    pos = random.randint(0,rank-1)
+    return pos
+
+def load_example(number):
+    url = f'./data/example-{number}.txt'
+    file = open(url,'r')
+    content = file.readlines()
+    points = []
+    for data in content:
+        x,y = data.split(" ")
+        points.append([int(x),int(y)])
+    distance = calculate_distance(points)
+    
+    return points,distance
+
+def draw_picture(points,distance,path,iteration):
+    rank = len(points)
+    ant_number = len(path)
+    x = []
+    y = []
+    for i in range(rank):
+        x.append(points[i][0])
+        y.append(points[i][1])
+    plt.scatter(x,y)
+    
+    min_cost = np.inf
+    for i in range(ant_number):
+        temp_cost = 0
+        for j in range(1,rank):
+            temp_cost+=distance[path[i][j-1]][path[i][j]]
+        temp_cost+=distance[path[i][0]][path[i][-1]]
+        if temp_cost<min_cost:
+            min_cost = temp_cost
+            best_path = path[i]
+        
+    for i in range(ant_number):
+        for j in range(rank):
+            x[j] = points[path[i][j]][0]
+            y[j] = points[path[i][j]][1]
+        plt.plot(x,y)
+
+    plt.text(0, 0, f'iteration:{iteration} min_cost = {round(min_cost,2)}', family='fantasy', fontsize=12,style='italic',color='mediumvioletred')
+    return round(min_cost,2),best_path
+
+
+def save_best_result(cost,path,points):
+    for i in range(1,len(path)):
+        x1 = points[path[i-1]][0]
+        y1 = points[path[i-1]][1]
+        x2 = points[path[i]][0]
+        y2 = points[path[i]][1]
+        plt.arrow(x1, y1, x2 - x1, y2 - y1, width = 0.05,color='r', length_includes_head=True)
+    plt.arrow(x2,y2,points[path[0]][0]-x2,points[path[0]][1]-y2, width = 0.05,color='r', length_includes_head=True)
+    #plt.text(0, 0, f'min_cost = {cost}')
+    plt.savefig("result.png")
+    plt.close()
+
+    print(f'best path = {show_path(path)}')
+
+def show_path(path):
+    route = str(path[0])
+    for i in range(1,len(path)):
+        route = route+" -> "+str(path[i])
+    route = route+"->"+str(path[0])
+    return route
