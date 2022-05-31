e8d6711fe64dabfe1a4d58fb2e3166ced567940b
windwize <91597193+windwize@users.noreply.github.com>
2022-05-31 22:45:02 +0900

    1

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ def default_argument_parser():
     parser.add_argument("--test",nargs="?")
     parser.add_argument('--ant', default=10,type=int)
     parser.add_argument('--points', default=20,type=int)
-    parser.add_argument('--generation', default=100,type=int) #[]
+    parser.add_argument('--generation', default=200,type=int) #[]
     parser.add_argument('--alpha', default=2.0,type=float)     #[1,4]
     parser.add_argument('--beta', default=3.0,type=float)     #[0,5]
-    parser.add_argument('--rho', default=0.5,type=float)       #[0.2,0.5]
+    parser.add_argument('--rho', default=0.3,type=float)       #[0.2,0.5]
     parser.add_argument('--q', default=100,type=float)
     parser.add_argument('--strategy', default=2,type=int) 
     parser.add_argument('--min_x', default=0,type=int) 
